4116be5ce14b4c2dd6f644c9ff3940df3f1ca004
Apurba Debnath <62664361+apurba2002@users.noreply.github.com>
2023-08-04 22:28:14 +0600

    zczczc

--- frontend/src/navbar.js
+++ frontend/src/navbar.js
@@ -22,9 +22,32 @@ function Navbar() {
                 </div>
 
             </div>
+            <div className="nav-res">
+                <div className="nav-show">
+                    <h1>Investo</h1>
+                    <img alt="" onClick={show} src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png"></img>
+
+
+                </div>
+
+            </div>
+            <div className="nav-hide" >
+                <a href="#" onClick={show}>Home</a>
+                <a href="#baner" onClick={show}>How it works</a>
+                <a href="#project" onClick={show}>Projects</a>
+                <a href="#about" onClick={show}>About Us</a>
+                <a href="#footer" onClick={show}>Contact Us</a>
+                <button onClick={show}>
+                    Sign up
+                </button>
+
+            </div>
 
 
         </>
     )
+}
+function show() {
+    document.querySelector(".nav-hide").classList.toggle('show');
 }
 export default Navbar
